4bcd83cf75422c7618e12a3bf2beb562ae50b501
David Conner <dconner.pro@gmail.com>
2015-09-30 15:20:09 -0400

    set up BufferProvider to be the interface through which buffers are accessed
    * SingleBufferProvider provides a single buffer through the same interface

--- Spectra/SpectraBufferProvider.swift
+++ Spectra/SpectraBufferProvider.swift
@@ -19,34 +19,40 @@ import Metal
 
 let inflightBuffersCountDefault = 3 // three is magic number
 
+// N.B. planning on always accessing Buffer via BufferProvider (and MTLBuffer via Buffer)
+// - so that interface is consistent and so that stuff can easily be built on top of one thing
+
 protocol BufferProvider {
     var bytecount:Int { get set }
     var availableBuffersIndex:Int { get set }
-    var availableBuffersSemaphore:dispatch_semaphore_t { get set }
-    var buffers: [MTLBuffer] { get set } // private
+    var availableBuffersSemaphore:dispatch_semaphore_t? { get set }
+    var buffers: [Buffer] { get set } // private
     
     init(device:MTLDevice, bytecount:Int, numInflightBuffers:Int, options:MTLResourceOptions)
     // deinit N.B. must deinit!
     
-    func nextBuffer() -> MTLBuffer
+    func getBuffer() -> Buffer
 }
 
-class BaseBufferProvider: BufferProvider {
+class BufferPoolProvider: BufferProvider {
     // init with max bytecount needed
     // - E.G. in case # of vertices needed changes
     var bytecount:Int
     var availableBuffersIndex:Int = 0
-    var availableBuffersSemaphore:dispatch_semaphore_t
-    var buffers: [MTLBuffer] = []
+    var availableBuffersSemaphore:dispatch_semaphore_t?
+    var buffers: [Buffer] = []
     var inflightBuffersCount:Int
     
-    required init(device:MTLDevice, bytecount:Int, numInflightBuffers:Int = inflightBuffersCountDefault, options: MTLResourceOptions = .CPUCacheModeDefaultCache) {
+    required init(device: MTLDevice, bytecount: Int, numInflightBuffers: Int = inflightBuffersCountDefault, options: MTLResourceOptions = .CPUCacheModeDefaultCache) {
         self.bytecount = bytecount
         inflightBuffersCount = numInflightBuffers
         availableBuffersSemaphore = dispatch_semaphore_create(inflightBuffersCount)
         
         for i in 0...inflightBuffersCount-1 {
-            buffers.append(device.newBufferWithLength(bytecount, options: options))
+            let thisBuffer = BaseBuffer()
+            thisBuffer.bytecount = self.bytecount
+            thisBuffer.prepareBuffer(device, options: options)
+            buffers.append(thisBuffer)
         }
     }
     
@@ -54,11 +60,11 @@ class BaseBufferProvider: BufferProvider {
         for i in 0...inflightBuffersCount-1 {
             // TODO: is this correct? this deinit's the same thing over and over
             // TODO: dealloc command buffers?
-            dispatch_semaphore_signal(self.availableBuffersSemaphore)
+            dispatch_semaphore_signal(self.availableBuffersSemaphore!)
         }
     }
     
-    func nextBuffer() -> MTLBuffer {
+    func getBuffer() -> Buffer {
         var buffer = buffers[availableBuffersIndex]
         
         availableBuffersIndex = (availableBuffersIndex + 1) % inflightBuffersCount
@@ -66,5 +72,31 @@ class BaseBufferProvider: BufferProvider {
         // return the buffer unmodified and let the user determine how to write to it
         return buffer
     }
+}
+
+class SingleBufferProvider: BufferProvider {
+    var bytecount:Int
+    var availableBuffersIndex:Int = 0
+    var availableBuffersSemaphore:dispatch_semaphore_t?
+    var buffers: [Buffer] = []
+    var inflightBuffersCount:Int
+    
+    required init(device:MTLDevice, bytecount:Int, numInflightBuffers:Int = 1, options: MTLResourceOptions = .StorageModeManaged) {
+        self.bytecount = bytecount
+        inflightBuffersCount = numInflightBuffer
+        prepareBuffer(device, options: options)
+    }
+    
+    func prepareBuffer(device:MTLDevice, options:MTLResourceOptions) {
+        let singleBuffer = BaseBuffer()
+        singleBuffer.bytecount = bytecount
+        singleBuffer.prepareBuffer(device, options: options)
+        buffers = [singleBuffer]
+    }
+    
+    func getBuffer() -> Buffer {
+        return buffers.first!
+    }
+    
 }
 
