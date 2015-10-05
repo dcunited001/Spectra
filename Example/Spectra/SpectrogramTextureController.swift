cc64e90fe1eaceabb34273689cae6694c37f98bc
David Conner <dconner.pro@gmail.com>
2015-10-05 00:49:15 -0400

    add a controller to my example

---
+++ Example/Spectra/SpectrogramTextureController.swift
@@ -0,0 +1,17 @@
+//
+//  SpectrogramController.swift
+//  Spectra
+//
+//  Created by David Conner on 10/5/15.
+//  Copyright © 2015 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import MetalKit
+import EZAudio
+
+
+class SpectrogramTextureController: MetalViewController {
+    
+}
+
