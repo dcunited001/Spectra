b038ffe32eb692c97359d680dd5882e34e9c0a01
David Conner <dconner.pro@gmail.com>
2015-10-07 12:21:39 -0400

    added & implemented RenderStage

---
+++ Pod/Classes/RenderStage.swift
@@ -0,0 +1,54 @@
+//
+//  RenderStage.swift
+//  Pods
+//
+//  Created by David Conner on 10/7/15.
+//
+//
+
+import Foundation
+
+typealias NodeGroup = [String:Node]
+typealias RenderStageNodeSelect = ((NodeGroup, [String]) -> [Node])
+typealias RenderStageNodeEncodeBlock = ((MTLRenderCommandEncoder, Node) -> Void)
+typealias RenderStageEncodeBlock = ((Renderer, [Node], RenderStageNodeEncodeBlock?) -> Void)
+typealias RenderStageTransitionBlock = ((Renderer, Renderer?) -> Renderer)
+
+protocol RenderStage: class {
+    var nodes: [Node] { get set }
+    var nodeSelectBlock: RenderStageNodeSelect? { get set }
+    var encodeBlock: RenderStageEncodeBlock? { get set }
+    var nodeEncodeBlock: RenderStageNodeEncodeBlock? { get set }
+    var transitionBlock: RenderStageTransitionBlock? { get set }
+    
+    func selectNodes(nodeGroup: NodeGroup, keys: [String])
+    func encode(renderer: Renderer, nodes: [Node])
+    func transition(renderer: Renderer, nextRenderer: Renderer?) -> Renderer
+}
+
+extension RenderStage {
+    func selectNodes(nodeGroup: NodeGroup, keys: [String]) {
+        nodes = nodeSelectBlock!(nodeGroup, keys)
+    }
+    
+    func encode(renderer: Renderer, nodes: [Node]) {
+        encodeBlock!(renderer, nodes, nodeEncodeBlock)
+    }
+    
+    func transition(renderer: Renderer, nextRenderer: Renderer?) -> Renderer {
+        if let transitionTo = transitionBlock {
+            return transitionTo(renderer, nextRenderer)
+        } else {
+            return renderer.transitionTo(nextRenderer)
+        }
+    }
+}
+
+class BaseRenderStage: RenderStage {
+    var nodes: [Node] = []
+    var nodeSelectBlock: RenderStageNodeSelect?
+    var encodeBlock: RenderStageEncodeBlock?
+    var nodeEncodeBlock: RenderStageNodeEncodeBlock?
+    var transitionBlock: RenderStageTransitionBlock?
+}
+
