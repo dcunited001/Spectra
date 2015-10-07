43555eb2d4d023fe43d01e216f6bf7dc838c86f5
David Conner <dconner.pro@gmail.com>
2015-10-07 06:23:22 -0400

    Scene: added RenderDelegate and UpdateDelegate
    * added some notes about state machines, etc

--- Pod/Classes/Scene.swift
+++ Pod/Classes/Scene.swift
@@ -14,13 +14,18 @@ import Metal
 // - mutating state between renderers in the collection
 // - passes renderEncoder into encode(renderEncoder) for each renderer
 
-class Scene { // ViewDelegate
-    var renderers: [Renderer] = []
+
+//TODO: explore using a state machine for RenderStrategy, where states are dynamically defined for each renderer type, a default state transition is defined which calls endEncoding() and creates a new renderEncoder and custom state transitions can be defined to transition renderEncoders without creating new ones
+//TODO: exploring using a similar map for Update Objects, so top level controller can easily specify dynamic object behaviors without needing to subclass
+
+class Scene: RenderDelegate, UpdateDelegate {
+    var pipelineStateMap: [String:MTLRenderPipelineState] = [:]
+    var rendererMap: [String:Renderer] = [:]
+    var nodeMap: [String:[Node]] = [:]
     
     //    init() {
     //
     //    }
-    
     
 //    func setupRenderStrategy
     
@@ -30,4 +35,7 @@ class Scene { // ViewDelegate
         // - to create renderEncoder
     }
     
+    func updateObjects(timeSinceLastUpdate: CFTimeInterval) {
+        
+    }
 }
