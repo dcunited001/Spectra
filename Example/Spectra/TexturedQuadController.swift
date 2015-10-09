84270504c025abe2bc86efbaea81da23cffaade7
David Conner <dconner.pro@gmail.com>
2015-10-09 19:05:10 -0400

    prematurely add TexturedQuadController

---
+++ Example/Spectra/TexturedQuadController.swift
@@ -0,0 +1,16 @@
+//
+//  TexturedQuadController.swift
+//  Spectra
+//
+//  Created by David Conner on 10/9/15.
+//  Copyright © 2015 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import Spectra
+import EZAudio
+import simd
+
+class TexturedQuadController: UIViewController {
+    
+}
