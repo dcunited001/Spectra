f15b316f912654cf3e332f1b2c0122ef3a12175a
David Conner <dconner.pro@gmail.com>
2015-10-09 20:21:45 -0400

    Camable: rename protocol functions and add BaseCamera

--- Pod/Classes/Camable.swift
+++ Pod/Classes/Camable.swift
@@ -14,19 +14,29 @@ public protocol Camable: class {
     var camCenter:float4 { get set }
     var camUp:float4 { get set }
     
-    func setProjectableDefaults()
-    func calcProjectionMatrix() -> float4x4
+    func setCamDefaults()
+    func calcCam() -> float4x4
 }
 
 // TODO: must deinit resources?
 public extension Camable {
-    public func setProjectableDefaults() {
+    public func setCamDefaults() {
         camEye =    [0.0, 0.0, 0.0, 1.0] //position
         camCenter = [0.0, 0.0, 1.0, 1.0] //position
         camUp =     [0.0, 1.0, 0.0, 0.0] //direction
     }
     
-    public func calcProjectionMatrix() -> float4x4 {
+    public func calcCam() -> float4x4 {
         return Transform3D.lookAt(camEye, center: camCenter, up: camUp)
+    }
+}
+
+public class BaseCamera: Camable {
+    public var camEye: float4 = []
+    public var camCenter: float4 = []
+    public var camUp: float4 = []
+    
+    public init() {
+        setCamDefaults()
     }
 }
