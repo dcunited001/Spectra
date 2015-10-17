46117a94a560ec597d46a7d08a1e6bf61c5a7170
David Conner <dconner.pro@gmail.com>
2015-10-17 01:54:48 -0400

    add blank S3DXML spec

---
+++ Example/Tests/S3DXMLSpec.swift
@@ -0,0 +1,27 @@
+//
+//  S3DXMLSpec.swift
+//  Spectra
+//
+//  Created by David Conner on 10/17/15.
+//  Copyright © 2015 CocoaPods. All rights reserved.
+//
+
+import Spectra
+import Ono
+import Quick
+import Nimble
+
+class S3DXMLSpec: QuickSpec {
+    override func spec() {
+        
+        describe("S3DXML") {
+            
+        }
+        
+        describe("MTLFunction") {
+            it("can parse MTLFunctions into maps") {
+                
+            }
+        }
+    }
+}
