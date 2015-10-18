43158051823e7af96e29a529d400742de9fb3588
David Conner <dconner.pro@gmail.com>
2015-10-18 02:24:21 -0400

    S3DXML: added new test
    * added test XML resource, but may remove soon

--- Example/Tests/S3DXMLSpec.swift
+++ Example/Tests/S3DXMLSpec.swift
@@ -11,16 +11,35 @@ import Spectra
 import Ono
 import Quick
 import Nimble
+import Metal
 
 class S3DXMLSpec: QuickSpec {
     override func spec() {
         
-        describe("S3DXML") {
-            
+        let device = MTLCreateSystemDefaultDevice()
+        let library = device!.newDefaultLibrary()
+        let xmlData: NSData?
+        
+        let descriptorManager = SpectraDescriptorManager(library: library!)
+        
+        describe("SpectraDescriptorManager") {
+            it("parses enumGroups from XSD") {  expect(descriptorManager.mtlEnums["mtlStoreAction"]!.getValue("ClampToEdge")) == 1
+            }
         }
         
+//        describe("S3DXML") {
+//            
+//        }
+  
         describe("MTLFunction") {
-            it("can parse MTLFunctions into maps") {
+            it("can parse a MTLFunction") {
+                
+            }
+        }
+
+        describe("MTLVertexDescriptor") {
+            
+            it("can parse a MTLVertexDescriptor") {
                 
             }
         }
