4a1747dab66cb1cfc55bca9b1f54ac0b8fa681f2
David Conner <dconner.pro@gmail.com>
2015-10-19 05:51:15 -0400

    cube view controller: implementing descMan & stateMan to manage metal descriptors & states

--- Example/Spectra/CubeViewController.swift
+++ Example/Spectra/CubeViewController.swift
@@ -16,6 +16,8 @@ class CubeViewController: MetalViewController {
     
     @IBOutlet weak var spectraView: Spectra.BaseView! //TODO: weak?
     var scene: Spectra.Scene?
+    var descMan: Spectra.SpectraDescriptorManager!
+    var stateMan: Spectra.SpectraStateManager!
     
     // i wish i could more strongly constrain the keys for these maps
     // - but i can't create a protocol for enums 
@@ -28,23 +30,14 @@ class CubeViewController: MetalViewController {
 //    }
     
     let cubeKey = "spectra_cube"
-    
     var pipelineStateMap: Spectra.RenderPipelineStateMap = [:]
-    var depthStencilStateMap: Spectra.DepthStencilStateMap = [:]
-    var vertexDescriptorMap: Spectra.VertexDescriptorMap = [:]
-    static let renderFunctionMap: Spectra.RenderPipelineFunctionMap = [
-        "basic": ("basicColorVertex", "basicColorFragment"),
-        "colorShift": ("basicColorShiftedVertex", "basicColorFragment"),
-        "colorShiftContinuous": ("basicColorShiftedContinuousVertex", "basicColorFragment")
-    ]
-    
     var encodableDataMap: Spectra.SceneEncodableDataMap = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadVertexDescriptorMap()
-        loadPipelineStateMap()
-        loadDepthStencilMap()
+        stateMan = Spectra.SpectraStateManager()
+        loadDescriptorManager()
+        loadRenderPipelineStates()
         
         scene = Spectra.Scene()
         setupPerspective()
@@ -55,24 +48,26 @@ class CubeViewController: MetalViewController {
         setupScene()
     }
     
-    func loadVertexDescriptorMap() {
-        vertexDescriptorMap["common"] = VertexDescriptorGenerator.commonVertexDescriptor()
+    func loadDescriptorManager() {
+        let bundle = NSBundle(forClass: CubeViewController.self)
+        let xmlData: NSData = S3DXML.readXML(bundle, filename: "Spectra3D")
+        let xml = S3DXML(data: xmlData)
+        
+        descMan = Spectra.SpectraDescriptorManager(library: spectraView.defaultLibrary)
+        descMan = xml.parse(descMan)
     }
     
-    func loadPipelineStateMap() {
-        let pipelineGenerator = Spectra.RenderPipelineGenerator(library: spectraView.defaultLibrary)
-        pipelineStateMap = pipelineGenerator.generatePipelineMap(spectraView.device!, functionMap: CubeViewController.renderFunctionMap, setupDescriptor: { (desc) in
+    // add descriptorConfigBlock?
+    func loadRenderPipelineStates() {
+        let pipeGen = RenderPipelineGenerator(library: spectraView.defaultLibrary)
+        stateMan.renderPipelineStates = descMan.renderPipelineDescriptors.reduce(Spectra.RenderPipelineStateMap()) { (var hash, kv) in
+            let k = kv.0
+            var desc = kv.1.copy() as! MTLRenderPipelineDescriptor
             desc.colorAttachments[0].pixelFormat = .BGRA8Unorm
-            desc.vertexDescriptor = self.vertexDescriptorMap["common"]
-            return desc
-        })
-    }
-    
-    func loadDepthStencilMap() {
-        let depthStateDesc = MTLDepthStencilDescriptor()
-        depthStateDesc.depthCompareFunction = .Always
-        depthStateDesc.depthWriteEnabled = true
-        depthStencilStateMap["default"] = spectraView.device?.newDepthStencilStateWithDescriptor(depthStateDesc)
+            let state = pipeGen.generateFromDescriptor(spectraView.device!, pipelineStateDescriptor: desc)
+            hash[k] = state!
+            return hash
+        }
     }
     
     func setupPerspective() {
@@ -99,8 +94,7 @@ class CubeViewController: MetalViewController {
     }
     
     func setupObjects() {
-        // parse Cube.XML
-        
+// parse Cube.XML
 //        let cubeXml = try! xml["root"]["mesh"].withAttr("id", cubeKey)
 //        setupCube(cubeKey, xml: cubeXml)
     }
@@ -110,3 +104,18 @@ class CubeViewController: MetalViewController {
     }
     
 }
+
+//    static let renderFunctionMap: Spectra.RenderPipelineFunctionMap = [
+//        "basic": ("basicColorVertex", "basicColorFragment"),
+//        "colorShift": ("basicColorShiftedVertex", "basicColorFragment"),
+//        "colorShiftContinuous": ("basicColorShiftedContinuousVertex", "basicColorFragment")
+//    ]
+
+//    func loadPipelineStateMap() {
+//        let pipelineGenerator = Spectra.RenderPipelineGenerator(library: spectraView.defaultLibrary)
+//        pipelineStateMap = pipelineGenerator.generatePipelineMap(spectraView.device!, functionMap: CubeViewController.renderFunctionMap, setupDescriptor: { (desc) in
+//            desc.colorAttachments[0].pixelFormat = .BGRA8Unorm
+//            desc.vertexDescriptor = self.vertexDescriptorMap["common"]
+//            return desc
+//        })
+//    }
