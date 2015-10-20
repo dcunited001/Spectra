4a87ddd32d362f4552ebec6dd4d4c427109cccb7
David Conner <dconner.pro@gmail.com>
2015-10-20 11:14:57 -0400

    SceneGraphXML: implementin' things

--- Example/Tests/SceneGraphXMLSpec.swift
+++ Example/Tests/SceneGraphXMLSpec.swift
@@ -12,9 +12,85 @@ import Ono
 import Quick
 import Nimble
 import Metal
+import simd
+
+class CustomView: Spectra.BaseWorldView {
+    var customVar: String?
+}
+
+class CustomCamera: Spectra.BaseCamera {
+    var customVar: String?
+}
+
+func compareFloat4(x:float4, to:float4) -> Bool {
+    //no == ????
+    //wow that's dumb. or maybe i'm dumb?
+    return (x.x == to.x && x.y == to.y && x.z == to.z && x.w == to.w)
+}
 
 class SceneGraphXMLSpec: QuickSpec {
+    
     override func spec() {
+        let testBundle = NSBundle(forClass: S3DXMLSpec.self)
+        let xmlData: NSData = SceneGraphXML.readXML(testBundle, filename: "SceneGraphXMLTest")
+        let sceneGraphXML = SceneGraphXML(data: xmlData)
         
+        var sceneGraph = SceneGraph()
+        sceneGraph.registerViewMonad("custom") {
+            let v = CustomView()
+            v.customVar = "custom"
+            return v
+        }
+        sceneGraph.registerCameraMonad("custom") {
+            let c = CustomCamera()
+            c.customVar = "custom"
+            return c
+        }
+        sceneGraph = sceneGraphXML.parse(sceneGraph)
+        
+        let defaultUniformsPos = float4(0,0,1,1)
+        let defaultUniformsRotation = float4(1,0,0,0)
+        let defaultUniformsScale = float4(1,1,1,0)
+        
+        describe("SGXMLUniformsNode") {
+            it("parses uniforms nodes") {
+                let u = sceneGraph.views["world"]!.uniforms
+                
+                expect(compareFloat4(u.uniformPosition, to: defaultUniformsPos)).to(beTrue())
+                expect(compareFloat4(u.uniformRotation, to: defaultUniformsRotation)).to(beTrue())
+                expect(compareFloat4(u.uniformScale, to: defaultUniformsScale)).to(beTrue())
+            }
+        }
+        
+        describe("SGXMLViewNode") {
+            it("parses default nodes") {
+                let u = sceneGraph.views["world"]!.uniforms
+                
+                expect(compareFloat4(u.uniformPosition, to: defaultUniformsPos)).to(beTrue())
+                expect(compareFloat4(u.uniformRotation, to: defaultUniformsRotation)).to(beTrue())
+                expect(compareFloat4(u.uniformScale, to: defaultUniformsScale)).to(beTrue())
+            }
+            
+            it("allows for custom implementations of Uniformable via viewMonads") {
+                let customWorld = sceneGraph.views["customWorld"] as! CustomView
+                expect(customWorld.customVar) == "custom"
+            }
+        }
+        
+        describe("SGXMLCameraNode") {
+            it("parses default nodes") {
+                
+            }
+            
+            it("allows for custom implementations of Uniformable via viewMonads") {
+                let customCam = sceneGraph.cameras["customCamera"] as! CustomCamera
+                expect(customCam.customVar) == "custom"
+            }
+        }
+        
+        describe("SGXMLPerspectiveNode") {
+            
+        }
     }
+    
 }
--- Pod/S3DXML/SceneGraphXML.swift
+++ Pod/S3DXML/SceneGraphXML.swift
@@ -9,6 +9,7 @@
 import Foundation
 import Ono
 import Metal
+import simd
 
 public class SceneGraphXML {
     public var xml: ONOXMLDocument?
@@ -17,9 +18,177 @@ public class SceneGraphXML {
         xml = try! ONOXMLDocument(data: data)
     }
     
-    public func parse(sceneGraph: SceneGraph) -> SceneGraph {
-        for child in xml!.rootElement.children {
+    public func parse(sceneGraph: SceneGraph, options: [String: AnyObject] = [:]) -> SceneGraph {
+        for child in xml!.rootElement.children {            
+            let elem = child as! ONOXMLElement
+            let tag = elem.tag
+            let key = elem.valueForAttribute("key") as? String
             
+            switch tag {
+            case "view":
+                sceneGraph.views[key!] = sceneGraph.views[key!] ?? SGXMLViewNode().parse(sceneGraph, elem: elem)
+            case "perspective":
+                sceneGraph.perspectives[key!] = sceneGraph.perspectives[key!] ?? SGXMLPerspectiveNode().parse(sceneGraph, elem: elem)
+            case "camera":
+                sceneGraph.cameras[key!] = sceneGraph.cameras[key!] ?? SGXMLCameraNode().parse(sceneGraph, elem: elem)
+            default:
+                break
+            }
         }
+        
+        return sceneGraph
+    }
+    
+    public class func readXML(bundle: NSBundle, filename: String, bundleResourceName: String? = nil) -> NSData {
+        
+        var resourceBundle: NSBundle = bundle
+        if let resourceName = bundleResourceName {
+            let bundleURL = bundle.URLForResource(resourceName, withExtension: "bundle")
+            resourceBundle = NSBundle(URL: bundleURL!)!
+        }
+        
+        let path = resourceBundle.pathForResource(filename, ofType: "xml")
+        let data = NSData(contentsOfFile: path!)
+        return data!
     }
 }
+
+public protocol SGXMLNodeParser {
+    typealias NodeType
+    
+    func parse(sceneGraph: SceneGraph, elem: ONOXMLElement, options: [String: AnyObject]) -> NodeType
+}
+
+public class SGXMLSimd {
+    public static func parseDoubles(str: String) -> [Double] {
+        var valStrs = str.characters.split { $0 == " " }.map(String.init)
+        return valStrs.map() { Double($0)! }
+    }
+    
+    public static func parseInts(str: String) -> [Int] {
+        var valStrs = str.characters.split { $0 == " " }.map(String.init)
+        return valStrs.map() { Int($0)! }
+    }
+    
+    public static func parseInt32s(str: String) -> [Int32] {
+        var valStrs = str.characters.split { $0 == " " }.map(String.init)
+        return valStrs.map() { Int32($0)! }
+    }
+    
+    public static func parseFloats(str: String) -> [Float] {
+        var valStrs = str.characters.split { $0 == " " }.map(String.init)
+        return valStrs.map() { Float($0)! }
+    }
+    
+    public static func parseFloat4(str: String) -> float4 {
+        return float4(parseFloats(str))
+    }
+    
+    public static func parseInt4(str: String) -> int4 {
+        return int4(parseInt32s(str))
+    }
+}
+
+
+public class SGXMLUniformsNode: SGXMLNodeParser {
+    public typealias NodeType = Uniformable
+    
+    public func parse(sceneGraph: SceneGraph, elem: ONOXMLElement, options: [String : AnyObject] = [:]) -> NodeType {
+        var node = BaseUniforms()
+        
+        if let pos = elem.valueForAttribute("pos") as? String {
+            node.uniformPosition = SGXMLSimd.parseFloat4(pos)
+        }
+        
+        if let rotation = elem.valueForAttribute("rotation") as? String {
+            node.uniformRotation = SGXMLSimd.parseFloat4(rotation)
+        }
+        
+        if let scale = elem.valueForAttribute("scale") as? String {
+            node.uniformScale = SGXMLSimd.parseFloat4(scale)
+        }
+        
+        return node
+    }
+}
+
+// TODO: how to instantiate protocol types without explicitly referring to the implementation of the protocol??
+// E.G. Uniforms/BaseUniforms
+public class SGXMLViewNode: SGXMLNodeParser {
+    public typealias NodeType = WorldView
+    
+    public func parse(sceneGraph: SceneGraph, elem: ONOXMLElement, options: [String: AnyObject] = [:]) -> NodeType {
+        
+        var node: NodeType
+        if let nodeType = elem.valueForAttribute("type") as? String {
+            if let monad = sceneGraph.getViewMonad(nodeType) {
+                node = monad()
+            } else {
+                node = BaseWorldView()
+            }
+        } else {
+            node = BaseWorldView()
+        }
+        
+        if let uniforms = elem.firstChildWithTag("uniforms") {
+            node.uniforms = SGXMLUniformsNode().parse(sceneGraph, elem: uniforms)
+        }
+        
+        return node
+    }
+}
+
+public class SGXMLCameraNode: SGXMLNodeParser {
+    public typealias NodeType = Camable
+    
+    public func parse(sceneGraph: SceneGraph, elem: ONOXMLElement, options: [String : AnyObject] = [:]) -> NodeType {
+        
+        var node: NodeType
+        if let nodeType = elem.valueForAttribute("type") as? String {
+            if let monad = sceneGraph.getCameraMonad(nodeType) {
+                node = monad()
+            } else {
+                node = BaseCamera()
+            }
+        } else {
+            node = BaseCamera()
+        }
+        
+        if let eye = elem.valueForAttribute("eye") as? String {
+            node.camEye = SGXMLSimd.parseFloat4(eye)
+        }
+        
+        if let center = elem.valueForAttribute("center") as? String {
+            node.camCenter = SGXMLSimd.parseFloat4(center)
+        }
+        
+        if let up = elem.valueForAttribute("up") as? String {
+            node.camUp = SGXMLSimd.parseFloat4(up)
+        }
+        
+        return node
+    }
+}
+
+public class SGXMLPerspectiveNode: SGXMLNodeParser {
+    public typealias NodeType = Perspectable
+    
+    public func parse(sceneGraph: SceneGraph, elem: ONOXMLElement, options: [String : AnyObject] = [:]) -> NodeType {
+        var node = BasePerspectable()
+        
+        if let type = elem.valueForAttribute("type") as? String {
+            node.perspectiveType = type
+        }
+        
+        let argsSelector = "perspective-arg"
+        elem.enumerateElementsWithCSS(argsSelector) {(el, idx, stop) in
+            let argName = el.valueForAttribute("name") as! String
+            let argValue = el.valueForAttribute("value") as! String
+            
+            node.perspectiveArgs[argName] = Float(argValue)
+        }
+        
+        return node
+    }
+}
+
