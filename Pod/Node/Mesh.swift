5be24ae48ec8809a597ace02457fc2b716eca74a
David Conner <dconner.pro@gmail.com>
2015-10-22 12:31:43 -0400

    corrected Mesh protocol

--- Pod/Node/Mesh.swift
+++ Pod/Node/Mesh.swift
@@ -10,10 +10,10 @@ import simd
 
 public protocol Mesh {
     var data: [String: [float4]] { get set }
-    var dataMaps: [String: [float4]] { get set }
+    var dataMaps: [String: [[Int]]] { get set }
 }
 
 public class BaseMesh: Mesh {
     public var data: [String: [float4]] = [:]
-    public var dataMaps: [String: [float4]] = [:]
+    public var dataMaps: [String: [[Int]]] = [:]
 }
